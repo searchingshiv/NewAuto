84e12766632e8d77c402dc71176dc2802fb6850a
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2024-05-13 20:27:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Abhibotssz/filter.git /Souravmkv/AutoFilerAdv 
+  git clone https://github.com/searchingshiv/NewAuto.git /searchingshiv/NewAuto 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Abhibotssz/filter 
+  git clone $UPSTREAM_REPO /NewAuto
 fi
-cd /Auto-filter 
+cd /NewAuto
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
